ab3283612c1a8b91c94b2b30b21968910252ac8a
chairma <cherma2012@mrcooper.com>
2020-08-10 16:30:40 +0530

    nested api with await

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -4,7 +4,7 @@ class TodoList extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {todos: []};
+    this.state = {posts: []};
   }
 
   componentDidMount = async () => { //Parent function needs to be have async keyword it you have await in it
@@ -15,19 +15,22 @@ class TodoList extends React.Component {
       //await to finish api and return data
       const todos = await fetch('https://jsonplaceholder.typicode.com/todos').then(response => response.json());
       console.log('API Done', todos);
-      return todos;
+      const firstUserId= todos[0].userId;
+      const posts =await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${firstUserId}`).then(response => response.json());
+
+      return posts;
     };
 
-    const todos = await makeApi(); //await used to make actions sync, 
+    const posts = await makeApi(); //await used to make actions sync, 
                                   //remove await n you'll see error saying todo is not iteratable
-    this.setState({ todos: [...todos]});
+    this.setState({ posts: [...posts]});
   }
 
   render() {
     return (
       <div className="list">
-        {this.state.todos.length === 0 && <div> Loading todos....</div> }
-        { this.state.todos.map(todo => <div key={todo.id} className="todo-item">{todo.title}</div> )}
+        {this.state.posts.length === 0 && <div> Loading posts....</div> }
+        { this.state.posts.map(post => <div key={post.id} className="post-item">{post.title}</div> )}
       </div>
     )
   }
